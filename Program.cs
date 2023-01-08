9426c98ccf794027087b4a690f11a52fd0f497e1
Guilherme Silva <guilhermes02@outlook.com.br>
2023-01-08 11:45:51 -0300

    Criacao do projeto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int a = 1;
+int b = 2;
+
+System.Console.WriteLine(Somar());      
+
+int Somar() => a+b;
